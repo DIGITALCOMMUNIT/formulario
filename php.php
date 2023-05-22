9961e4ef9276aab2f02d5f86c95581ac8dc6bde6
DIGITALCOMMUNIT <bryan_matute@yahoo.com>
2023-05-22 16:41:13 -0600

    Update php.php
    opcion

--- php.php
+++ php.php
@@ -1,18 +1,42 @@
 <?php
-if(isset($_POST['submit'])) {
-    $to = "digitalcommunity3@gmail.com"; // Enter your email address here
-    $subject = "Restaurant Survey Response";
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $experience = $_POST['experience'];
-    $food_quality = $_POST['food-quality'];
-    $service = $_POST['service'];
-    $atmosphere = $_POST['atmosphere'];
-    $recommendation = $_POST['recommendation'];
-    $comments = $_POST['comments'];
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-    $message = "Name: ".$name."\nEmail: ".$email."\nOverall Experience: ".$experience."\nFood Quality: ".$food_quality."\nService: ".$service."\nAtmosphere: ".$atmosphere."\nWould you recommend us to others?: ".$recommendation."\nAdditional Comments: ".$comments;
+require 'vendor/autoload.php';
 
-    mail($to, $subject, $message);
+// Recuperar los datos del formulario
+$nombre = $_POST['name'];
+$email = $_POST['email'];
+$experiencia = $_POST['experience'];
+$calidad_comida = $_POST['food-quality'];
+$servicio = $_POST['service'];
+$ambiente = $_POST['atmosphere'];
+$recomendacion = $_POST['recommendation'];
+$comentarios = $_POST['comments'];
+
+// Crear una nueva instancia de PHPMailer
+$mail = new PHPMailer(true);
+
+try {
+    // Configurar el servidor SMTP
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'ai.bryan.auto@gmail.com';
+    $mail->Password = 'anryb2323';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+
+    // Configurar el correo electrónico
+    $mail->setFrom('ai.beyan.auto@gmail.com', 'prueva');
+    $mail->addAddress('digitalcommunity3@gmail.com', 'Destinatario');
+    $mail->Subject = 'Respuestas de la encuesta del restaurante';
+    $mail->Body = "Nombre: $nombre\nEmail: $email\nExperiencia general: $experiencia\nCalidad de la comida: $calidad_comida\nServicio: $servicio\nAmbiente: $ambiente\n¿Recomendaría nuestro restaurante?: $recomendacion\nComentarios adicionales:\n$comentarios";
+
+    // Enviar el correo electrónico
+    $mail->send();
+    echo 'El correo electrónico ha sido enviado.';
+} catch (Exception $e) {
+    echo 'El correo electrónico no pudo ser enviado. Error: ', $mail->ErrorInfo;
 }
 ?>
